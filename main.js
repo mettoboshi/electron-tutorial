3cb2ed177e86702af7c9224e1ab313cebde543fc
Yasuo Fujita <mettoboshi.1108@gmail.com>
2021-10-19 23:23:48 +0900

    プリロードスクリプトを使ってレンダラーから Node.js にアクセスする

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const { app, BrowserWindow } = require('electron')
 
+// ファイルの先頭で Node.js の 'path' モジュールをインクルードします
+const path = require('path')
+
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
